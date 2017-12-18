c5d27b85dd71d140f9d96b133dc603cb95046a30
Ryan Ferris and Sam Gespass <ryan@>
2017-12-18 10:11:15 -0800

    add front end logic to create x3 test pet object instances.

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,8 @@ function Pet(name, breed, age, color, sex, personality, picture) {
 
 
 /*FRONT END LOGIC*/
+var dog = new Pet("Felipe", ["Dog", "Terrier Mix"], "5 months", ["black", "white"], "male", "energetic", "'<img src='img/felipe.jpg></img>'");
+
+var cat = new Pet("Alice", ["Cat", "American Short-Hair"], "4 years", ["calico"], "female", "affectionate", "'<img src='img/alice.jpg></img>'");
+
+var fish = new Pet("Zero", ["Fish", "Betta"], "1 year", ["blue", "red"], "male", "energetic", "'<img src='img/zero.jpg></img>'");
