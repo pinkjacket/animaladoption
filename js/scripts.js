595b0105dd28342a9620a467e55dddcf6d24cd7f
Ryan Ferris and Sam Gespass <ryan@>
2017-12-18 09:41:12 -0800

    add back end logic for the Pet constructor.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/*BACK END LOGIC*/
+function Pet(name, breed, age, color, sex, personality, picture) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+  this.color = color;
+  this.sex = sex;
+  this.personality = personality;
+  this.picture = picture;
+  this.adoptionStatus = false;
+}
+
+
+
+
+/*FRONT END LOGIC*/
