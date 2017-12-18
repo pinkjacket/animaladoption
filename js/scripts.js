09c83a34be7820465c20480e7a9c9cfd50a94c3f
Ryan Ferris and Sam Gespass <ryan@>
2017-12-18 10:21:44 -0800

    fix images to be self-closing

--- js/scripts.js
+++ js/scripts.js
@@ -12,10 +12,9 @@ function Pet(name, breed, age, color, sex, personality, picture) {
 
 
 
-
 /*FRONT END LOGIC*/
-var dog = new Pet("Felipe", ["Dog", "Terrier Mix"], "5 months", ["black", "white"], "male", "energetic", "'<img src='img/felipe.jpg></img>'");
+var dog = new Pet("Felipe", ["Dog", "Terrier Mix"], "5 months", ["black", "white"], "male", "energetic", "<img src='img/felipe.jpg' />");
 
-var cat = new Pet("Alice", ["Cat", "American Short-Hair"], "4 years", ["calico"], "female", "affectionate", "'<img src='img/alice.jpg></img>'");
+var cat = new Pet("Alice", ["Cat", "American Short-Hair"], "4 years", ["calico"], "female", "affectionate", "<img src='img/alice.jpg' />");
 
-var fish = new Pet("Zero", ["Fish", "Betta"], "1 year", ["blue", "red"], "male", "energetic", "'<img src='img/zero.jpg></img>'");
+var fish = new Pet("Zero", ["Fish", "Betta"], "1 year", ["blue", "red"], "male", "energetic", "<img src='img/zero.jpg' />");
