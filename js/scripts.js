00cb6cdb95d2eaebb54bc699cfa01eee858d15f2
Adam Calhoun and James Donlan <adam@>
2017-12-18 10:54:28 -0800

    display pet pictures

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,20 @@ function Pet(name, breed, age, color, sex, personality, picture) {
 
 
 /*FRONT END LOGIC*/
-var dog = new Pet("Felipe", ["Dog", "Terrier Mix"], "5 months", ["black", "white"], "male", "energetic", "<img src='img/felipe.jpg' />");
 
-var cat = new Pet("Alice", ["Cat", "American Short-Hair"], "4 years", ["calico"], "female", "affectionate", "<img src='img/alice.jpg' />");
+$(document).ready(function(){
+  var dog = new Pet("Felipe", ["Dog", "Terrier Mix"], "5 months", ["black", "white"], "male", "energetic", "<img src='img/felipe.jpg' />");
+  var cat = new Pet("Alice", ["Cat", "American Short-Hair"], "4 years", ["calico"], "female", "affectionate", "<img src='img/alice.jpg' />");
+  var fish = new Pet("Zero", ["Fish", "Betta"], "1 year", ["blue", "red"], "male", "energetic", "<img src='img/zero.jpg' />");
 
-var fish = new Pet("Zero", ["Fish", "Betta"], "1 year", ["blue", "red"], "male", "energetic", "<img src='img/zero.jpg' />");
+  var petList = [dog, cat, fish];
+
+  for (var i = 0; i<petList.length;i++){
+    $(".pets").append("<li>"+ petList[i].picture +"</li>")
+  }
+
+  $("#petBar").last().click(function(){
+    $(".info").empty();
+    $(".info").append($("#petBar").last().name)
+  });
+});
